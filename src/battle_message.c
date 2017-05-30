38140d1697c862a76ca71cb901225e69ac62a726
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2017-05-30 11:25:23 +0200

    Battle Message

--- src/battle_message.c
+++ src/battle_message.c
@@ -5,6 +5,7 @@
 #include "item.h"
 #include "items.h"
 #include "link.h"
+#include "moves.h"
 #include "rom_8077ABC.h"
 #include "string_util.h"
 #include "text.h"
@@ -52,6 +53,508 @@ struct UnkStructBattle {
 };
 
 extern const struct UnkStructBattle gUnknown_081FA6D4[];
+
+// <editor-fold desc="Description">
+extern const u8 gUnknown_083FEE5D[];
+extern const u8 gUnknown_083FEE92[];
+extern const u8 BattleText_PreventedS[];
+extern const u8 gUnknown_083FFCCA[];
+extern const u8 gUnknown_083FFDB3[];
+extern const u8 gUnknown_083FFDD3[];
+extern const u8 gUnknown_083FFEFC[];
+extern const u8 gUnknown_083FFF56[];
+extern const u8 gUnknown_083FFF6A[];
+extern const u8 gUnknown_083FFF81[];
+extern const u8 gUnknown_083FFF99[];
+extern const u8 gUnknown_083FFFB3[];
+extern const u8 gUnknown_083FFFCB[];
+extern const u8 gUnknown_083FFFEA[];
+extern const u8 gUnknown_083FFFF7[];
+extern const u8 gUnknown_08400555[];
+extern const u8 gUnknown_08400568[];
+extern const u8 gUnknown_0840057B[];
+extern const u8 gUnknown_08400590[];
+extern const u8 gUnknown_084005AA[];
+extern const u8 gUnknown_084005C7[];
+extern const u8 gUnknown_084005DB[];
+extern const u8 gUnknown_084005F5[];
+extern const u8 gUnknown_08400608[];
+extern const u8 gUnknown_08400622[];
+extern const u8 gUnknown_08400635[];
+extern const u8 gUnknown_08400645[];
+extern const u8 gUnknown_0840065C[];
+extern const u8 gUnknown_0840067C[];
+extern const u8 gUnknown_0840068C[];
+extern const u8 gUnknown_0840069C[];
+extern const u8 gUnknown_084006A4[];
+extern const u8 gUnknown_084006B3[];
+extern const u8 gUnknown_084006BB[];
+extern const u8 gUnknown_084006C6[];
+extern const u8 gUnknown_084006D5[];
+extern const u8 gUnknown_084006F1[];
+extern const u8 gUnknown_08400709[];
+extern const u8 gUnknown_08400727[];
+extern const u8 gUnknown_08400736[];
+extern const u8 gUnknown_08400749[];
+extern const u8 gUnknown_0840075E[];
+extern const u8 gUnknown_08400771[];
+extern const u8 gUnknown_08400781[];
+extern const u8 gUnknown_08400791[];
+extern const u8 gUnknown_08400797[];
+extern const u8 gUnknown_0840079C[];
+extern const u8 gUnknown_084007A1[];
+extern const u8 gUnknown_084007A7[];
+extern const u8 gUnknown_084007AC[];
+extern const u8 gUnknown_084007B2[];
+extern const u8 gUnknown_084007B7[];
+extern const u8 gUnknown_084007BD[];
+extern const u8 gUnknown_084007C8[];
+extern const u8 gUnknown_084007CA[];
+extern const u8 gUnknown_084007CC[];
+extern const u8 gUnknown_084007CE[];
+extern const u8 gUnknown_084007D0[];
+extern const u8 gUnknown_084009ED[];
+extern const u8 gUnknown_084009F7[];
+extern const u8 gUnknown_08400A78[];
+extern const u8 gUnknown_08400A85[];
+extern const u8 gUnknown_08400C4A[];
+extern const u8 gUnknown_08400C60[];
+extern const u8 gUnknown_08400C8D[];
+extern const u8 gUnknown_08400CA8[];
+extern const u8 gUnknown_08400CBB[];
+extern const u8 gUnknown_08400CCC[];
+extern const u8 gUnknown_08400CE0[];
+extern const u8 gUnknown_08400CF3[];
+extern const u8 gUnknown_08400D15[];
+extern const u8 gUnknown_08400D38[];
+extern const u8 gUnknown_08400D49[];
+extern const u8 gUnknown_08400D52[];
+extern const u8 gUnknown_08400D7A[];
+extern const u8 gUnknown_08400D89[];
+extern const u8 gUnknown_08400D9F[];
+extern const u8 gUnknown_08400DAA[];
+extern const u8 gUnknown_08400DAC[];
+extern const u8 gUnknown_08400DD6[];
+extern const u8 BattleText_SafariBall[];
+extern const u8 BattleText_SafariBall[];
+extern const u8 gUnknown_08400DE6[];
+extern const u8 gUnknown_08400DF0[];
+extern const u8 BattleText_HighlightR[];
+extern const u8 BattleText_Sleep[];
+extern const u8 BattleText_PoisonStat[];
+extern const u8 BattleText_Burn[];
+extern const u8 BattleText_Paralysis[];
+extern const u8 BattleText_IceStatus[];
+extern const u8 BattleText_Confusion[];
+extern const u8 BattleText_Love[];
+extern const u8 gUnknown_08400E23[];
+extern const u8 gUnknown_08400E29[];
+extern const u8 gUnknown_08400E2C[];
+extern const u8 gUnknown_08400E2E[];
+extern const u8 gUnknown_08400E30[];
+extern const u8 gUnknown_08400E32[];
+extern const u8 gUnknown_08400E36[];
+extern const u8 gBadEggNickname[];
+extern const u8 gUnknown_08400E42[];
+extern const u8 BattleText_Wally[];
+extern const u8 BattleText_Win[];
+extern const u8 BattleText_Loss[];
+extern const u8 BattleText_Tie[];
+extern const u8 gUnknown_08401508[];
+extern const u8 BattleTextList_401512[];
+extern const u8 BattleTextList_40151C[];
+extern const u8 BattleTextList_401528[];
+extern const u8 BattleTextList_40152C[];
+extern const u8 BattleTextList_401530[];
+extern const u8 BattleTextList_401534[];
+extern const u8 BattleTextList_40153A[];
+extern const u8 BattleTextList_401540[];
+extern const u8 BattleTextList_40154C[];
+extern const u8 BattleTextList_401556[];
+extern const u8 BattleTextList_40155A[];
+extern const u8 BattleTextList_40155E[];
+extern const u8 BattleTextList_401562[];
+extern const u8 BattleTextList_401566[];
+extern const u8 BattleTextList_40156A[];
+extern const u8 BattleTextList_401570[];
+extern const u8 BattleTextList_40157C[];
+extern const u8 BattleTextList_401584[];
+extern const u8 BattleTextList_401594[];
+extern const u8 BattleTextList_4015A0[];
+extern const u8 BattleTextList_4015A4[];
+extern const u8 BattleTextList_4015A8[];
+extern const u8 BattleTextList_4015AC[];
+extern const u8 BattleTextList_4015B0[];
+extern const u8 BattleTextList_4015B4[];
+extern const u8 BattleTextList_4015B8[];
+extern const u8 BattleTextList_4015BC[];
+extern const u8 BattleTextList_4015C0[];
+extern const u8 BattleTextList_4015C4[];
+extern const u8 BattleTextList_4015C8[];
+extern const u8 BattleTextList_4015D0[];
+extern const u8 BattleTextList_4015D4[];
+extern const u8 BattleTextList_4015D8[];
+extern const u8 BattleTextList_4015E2[];
+extern const u8 BattleTextList_4015E6[];
+extern const u8 BattleTextList_4015EE[];
+extern const u8 BattleTextList_40160E[];
+extern const u8 BattleTextList_401616[];
+extern const u8 BattleTextList_40161A[];
+extern const u8 BattleTextList_401620[];
+extern const u8 BattleTextList_40162C[];
+extern const u8 BattleTextList_401630[];
+extern const u8 BattleTextList_401636[];
+extern const u8 BattleTextList_40163C[];
+extern const u8 BattleTextList_401642[];
+extern const u8 BattleTextList_401648[];
+extern const u8 gUnknown_0840164C[];
+extern const u8 gUnknown_0840165C[];
+extern const u8 *gUnknown_08401674[];
+extern const u8 gUnknown_084017A8[];
+// </editor-fold>
+
+struct UnkStructA {
+    u16 var00;
+    u8 pad02[2];
+    u16 var04;
+    u8 var06;
+    u8 var07;
+    u8 var08;
+    u8 var09;
+    u8 var0A;
+    u8 var0B;
+    u8 var0C[4];
+    u8 var10[16];
+    u8 var20[16];
+    u8 var30[16];
+    u8 pad40[448];
+};
+
+extern u16 gUnknown_02024C04;
+extern u8 byte_2024C06;
+
+extern struct UnkStructA gUnknown_02023A64[];
+
+extern u16 gTrainerBattleOpponent;
+extern struct UnkStructA *gSelectedOrderFromParty;
+
+#define BATTLE_TEXT_NoEffectOn 362
+
+
+extern u8 gUnknown_02024A60;
+extern u8 gUnknown_030042B0[];
+extern u8 gUnknown_0203926C[];
+
+void sub_8121D1C(const u8 *arg0);
+
+void sub_8121D74(const u8 *dstPtr);
+
+void sub_8120AA8(u16 arg0) {
+    int i;
+    const u8 *ptr = NULL;
+
+    gSelectedOrderFromParty = &gUnknown_02023A64[gUnknown_02024A60];
+
+    gUnknown_02024C04 = gSelectedOrderFromParty->var04;
+    byte_2024C06 = gSelectedOrderFromParty->var06;
+    ewram[0x16003] = gSelectedOrderFromParty->var07;
+    ewram[0x1605E] = gSelectedOrderFromParty->var08;
+    ewram[0x160C1] = gSelectedOrderFromParty->var09;
+    gUnknown_02024C0B = gSelectedOrderFromParty->var0A;
+    ewram[0x160A0] = gSelectedOrderFromParty->var0B;
+
+    for (i = 0; i < 4; i++)
+    {
+        gUnknown_0203926C[i] = gSelectedOrderFromParty->var0C[i];
+    }
+
+
+    for (i = 0; i < 0x10; i++)
+    {
+        gUnknown_030041C0[i] = gSelectedOrderFromParty->var10[i];
+        gUnknown_03004290[i] = gSelectedOrderFromParty->var20[i];
+        gUnknown_030042B0[i] = gSelectedOrderFromParty->var30[i];
+    }
+
+    switch (arg0)
+    {
+    case 0:
+        if (gBattleTypeFlags & BATTLE_TYPE_TRAINER)
+        {
+            if (gBattleTypeFlags & BATTLE_TYPE_LINK)
+            {
+                if (!(gBattleTypeFlags & BATTLE_TYPE_MULTI))
+                {
+                    ptr = gUnknown_084005C7;
+                    break;
+                }
+
+                ptr = gUnknown_084005DB;
+            }
+            else
+            {
+                ptr = gUnknown_084005AA;
+            }
+        }
+        else
+        {
+            if (gBattleTypeFlags & BATTLE_TYPE_LEGENDARY)
+            {
+                ptr = gUnknown_08400568;
+            }
+            else if (gBattleTypeFlags & BATTLE_TYPE_DOUBLE)
+            {
+                ptr = gUnknown_08400590;
+            }
+            else if (gBattleTypeFlags & BATTLE_TYPE_WALLY_TUTORIAL)
+            {
+                ptr = gUnknown_0840057B;
+            }
+            else
+            {
+                ptr = gUnknown_08400555;
+            }
+        }
+
+        break;
+
+    case 1:
+        if (battle_side_get_owner(gUnknown_02024A60) == 0)
+        {
+            if (gBattleTypeFlags & BATTLE_TYPE_DOUBLE)
+            {
+                if (gBattleTypeFlags & BATTLE_TYPE_MULTI)
+                {
+                    ptr = gUnknown_084006F1;
+                }
+                else
+                {
+                    ptr = gUnknown_084006A4;
+                }
+            }
+            else
+            {
+                ptr = gUnknown_0840069C;
+            }
+        }
+        else
+        {
+            if (gBattleTypeFlags & BATTLE_TYPE_DOUBLE)
+            {
+                if (gBattleTypeFlags & BATTLE_TYPE_MULTI)
+                {
+                    ptr = gUnknown_0840065C;
+                }
+                else if (gBattleTypeFlags & BATTLE_TYPE_LINK)
+                {
+                    ptr = gUnknown_08400645;
+                }
+                else
+                {
+                    ptr = gUnknown_08400608;
+                }
+            }
+            else
+            {
+                if (gBattleTypeFlags & BATTLE_TYPE_LINK)
+                {
+                    ptr = gUnknown_08400635;
+                }
+                else
+                {
+                    ptr = gUnknown_084005F5;
+                }
+            }
+        }
+
+        break;
+
+    case 2:
+        if (battle_side_get_owner(gUnknown_02024A60) == 0)
+        {
+            if (ewram[0x160C1] == 0)
+            {
+                ptr = gUnknown_08400709;
+            }
+            else if (ewram[0x160C1] == 1 || (gBattleTypeFlags & BATTLE_TYPE_DOUBLE))
+            {
+                ptr = gUnknown_08400727;
+            }
+            else if (ewram[0x160C1] == 2)
+            {
+                ptr = gUnknown_08400736;
+            }
+            else
+            {
+                ptr = gUnknown_08400749;
+            }
+        }
+        else
+        {
+
+            if (gTrainerBattleOpponent == 0x800)
+            {
+                if (gBattleTypeFlags & BATTLE_TYPE_MULTI)
+                {
+                    ptr = gUnknown_08400781;
+                }
+                else
+                {
+                    ptr = gUnknown_08400771;
+                }
+            }
+            else
+            {
+                ptr = gUnknown_0840075E;
+            }
+        }
+
+        break;
+
+    case 3:
+        if (battle_side_get_owner(ewram[0x16003]) == 0)
+        {
+            if (ewram[0x160C1] == 0 || gBattleTypeFlags & BATTLE_TYPE_DOUBLE)
+            {
+                ptr = gUnknown_084006B3;
+            }
+            else if (ewram[0x160C1] == 1)
+            {
+                ptr = gUnknown_084006BB;
+            }
+            else if (ewram[0x160C1] == 2)
+            {
+                ptr = gUnknown_084006C6;
+            }
+            else
+            {
+                ptr = gUnknown_084006D5;
+            }
+        }
+        else
+        {
+            if (gBattleTypeFlags & BATTLE_TYPE_LINK)
+            {
+                if (gBattleTypeFlags & BATTLE_TYPE_MULTI)
+                {
+                    ptr = gUnknown_0840068C;
+                }
+                else
+                {
+                    ptr = gUnknown_0840067C;
+                }
+            }
+            else
+            {
+                ptr = gUnknown_08400622;
+            }
+        }
+
+        break;
+
+    case 4:
+        sub_8121D1C(gUnknown_030041C0);
+
+        if (gSelectedOrderFromParty->var00 > MOVE_PSYCHO_BOOST)
+        {
+            StringCopy(gUnknown_03004290, gUnknown_08401674[ewram[0x160A0]]);
+        }
+        else
+        {
+            StringCopy(gUnknown_03004290, gMoveNames[gSelectedOrderFromParty->var00]);
+        }
+
+        sub_8121D74(gUnknown_03004290);
+        ptr = gUnknown_084007BD;
+        break;
+
+    case 5:
+        if (gUnknown_030041C0[0] & 0x80)
+        {
+            gUnknown_030041C0[0] &= ~0x80;
+
+            if (battle_side_get_owner(gUnknown_02024A60) == 1 && gUnknown_030041C0[0] != 3)
+            {
+                gUnknown_030041C0[0] ^= 3;
+            }
+
+            if ((u8) (gUnknown_030041C0[0] - 2) < 2)
+            {
+                ptr = gUnknown_083FFEFC;
+            }
+            else
+            {
+                if (gBattleTypeFlags & BATTLE_TYPE_MULTI)
+                {
+                    ptr = gUnknown_083FFFF7;
+                }
+                else
+                {
+                    ptr = gUnknown_083FFFEA;
+                }
+            }
+        }
+        else
+        {
+            if (battle_side_get_owner(gUnknown_02024A60) == 1 && gUnknown_030041C0[0] != 3)
+            {
+                gUnknown_030041C0[0] ^= 3;
+            }
+
+
+            if (gBattleTypeFlags & BATTLE_TYPE_MULTI)
+            {
+                switch (gUnknown_030041C0[0])
+                {
+                case 1:
+                    ptr = gUnknown_083FFF6A;
+                    break;
+                case 2:
+                    ptr = gUnknown_083FFF99;
+                    break;
+                case 3:
+                    ptr = gUnknown_083FFFCB;
+                    break;
+                }
+            }
+            else
+            {
+                switch (gUnknown_030041C0[0])
+                {
+                case 1:
+                    ptr = gUnknown_083FFF56;
+                    break;
+                case 2:
+                    ptr = gUnknown_083FFF81;
+                    break;
+                case 3:
+                    ptr = gUnknown_083FFFB3;
+                    break;
+                }
+            }
+        }
+
+        break;
+
+    default:
+        if (arg0 > BATTLE_TEXT_NoEffectOn)
+        {
+            gUnknown_020238CC[0] = EOS;
+            return;
+        }
+
+        ptr = gUnknown_08400F8C[arg0 - 12];
+        break;
+    }
+
+    get_battle_strings_(ptr);
+}
+
+
+u8* get_battle_strings_(const u8 * arg0) {
+    return sub_8120FFC(arg0, gUnknown_020238CC);
+}
+
 
 #ifdef NONMATCHING
 const u8 *sub_8120F98(u8 *arg0) {
@@ -381,6 +884,7 @@ void sub_8121A68(u8 *r0, u8 *r1) {
         }
     }
 }
+asm("\t.align 2, 0\n");
 
 #ifdef NONMATCHING
 
